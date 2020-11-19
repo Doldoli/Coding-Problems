9ae8fc8b69a0c665b1cd1b2aaa3224d1392e755f
Gilbert <andre-anan.gilbert@sap.com>
2020-11-19 16:21:29 +0100

    added new problem

---
+++ Easy/Week 4/Problem 2/problem_2.py
@@ -0,0 +1,12 @@
+'''
+Week 4 - Problem 2 
+@author: André Gilbert, andre.gilbert.77110@gmail.com
+
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+'''
+
+# input = 38
+# output = 2
+# 3 + 8 = 11, 1 + 1 = 2 since 2 has only one digit, return it.
+
+num = 47
