17f0896371e4287d4bb49ab6d4e1007a30650e08
Gilbert <andre-anan.gilbert@sap.com>
2020-11-19 15:37:38 +0100

    add new problem

---
+++ Easy/Week 4/Problem 1/problem_1.py
@@ -0,0 +1,12 @@
+'''
+Week 4 - Problem 1 
+@author: André Gilbert, andre.gilbert.77110@gmail.com
+
+Given a string s consists of some words separated by spaces, 
+return the length of the last word in the string. If the last word does not exist, return 0.
+'''
+
+# input = "Hello World"
+# output: 5
+
+s = "Good morning"
